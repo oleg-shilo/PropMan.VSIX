6fc3ad54698c794d5531d416b221bb2559f4ea96
oleg.shilo <oleg.shilo@leicabiosystems.com>
2019-08-12 12:00:44 +1000

    Added missing files

---
+++ PropMan/PropManCommand.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace OlegShilo.PropMan
+{
+    static class GuidList
+    {
+        public const string guidPropManPkgString = "7c64986f-f4d6-4983-a49a-518d5512b212";
+        public const string guidPropManCmdSetString = "db2d94ca-ca87-4240-99ea-931d7f8e7701";
+        public const string guidPropManCmdConfigSetString = "db2d94ca-ca87-4240-99ea-931d7f8e7733";
+        public const string guidToolWindowPersistanceString = "fd0da38f-47b9-4072-b27d-01629b580799";
+
+        public static readonly Guid guidPropManCmdSet = new Guid(guidPropManCmdSetString);
+        public static readonly Guid guidPropManConfigCmdSet = new Guid(guidPropManCmdConfigSetString);
+    };
+
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class PropManCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("027fe469-162c-418e-8315-99f12cf9110b");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PropManCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private PropManCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+
+            // // Create the command for the menu item.
+            // CommandID menuCommandID = new CommandID(GuidList.guidPropManCmdSet, (int)PkgCmdIDList.cmdidOpenCommand);
+            // MenuCommand menuItem = new MenuCommand(MenuItemCallback, menuCommandID);
+            // mcs.AddCommand(menuItem);
+
+            // Create the command for the menu item.
+            // menuCommandID = new CommandID(GuidList.guidPropManConfigCmdSet, (int)PkgCmdIDList.cmdidOpenConfigCommand);
+            // menuItem = new MenuCommand(ShowToolWindow, menuCommandID);
+            // mcs.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static PropManCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in ToolWindow1Command's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new PropManCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// Shows the tool window when the menu item is clicked.
+        /// </summary>
+        /// <param name="sender">The event sender.</param>
+        /// <param name="e">The event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            // Get the instance number 0 of this tool window. This window is single instance so this instance
+            // is actually the only one.
+            // The last flag is set to true so that if the tool window does not exists it will be created.
+            ToolWindowPane window = this.package.FindToolWindow(typeof(MyToolWindow), 0, true);
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            IVsWindowFrame windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+    }
+}
---
+++ PropMan/RefactorPropertyCommand.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Interop;
+using Microsoft.AutoPropertyConverterVSX;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Task = System.Threading.Tasks.Task;
+
+namespace OlegShilo.PropMan
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class RefactorPropertyCommand
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 256;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("2566205f-f6ef-4a3e-8bd0-d0858a8737b8");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="RefactorPropertyCommand"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private RefactorPropertyCommand(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static RefactorPropertyCommand Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in RefactorPropertyCommand's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync((typeof(IMenuCommandService))) as OleMenuCommandService;
+            Instance = new RefactorPropertyCommand(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+            // MenuItemCallback();
+
+            // string message = string.Format(CultureInfo.CurrentCulture, "Inside {0}.MenuItemCallback()", this.GetType().FullName);
+            // string title = "RefactorPropertyCommand";
+
+            // // Show a message box to prove we were here
+            // VsShellUtilities.ShowMessageBox(
+            //     this.package,
+            //     message,
+            //     title,
+            //     OLEMSGICON.OLEMSGICON_INFO,
+            //     OLEMSGBUTTON.OLEMSGBUTTON_OK,
+            //     OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+
+            try
+            {
+                var txtxMgr = (IVsTextManager)GetService<SVsTextManager>();
+
+                IVsUIShell uiShell = (IVsUIShell)GetService<SVsUIShell>();
+                IntPtr mainWnd;
+                uiShell.GetDialogOwnerHwnd(out mainWnd);
+
+                var dialog = new Window1();
+                var helper = new WindowInteropHelper(dialog) { Owner = mainWnd };
+                dialog.ShowDialog();
+
+                if (dialog.SelectedOperation == Window1.Operation.AutoFull)
+                {
+                    var plugin = new AutoPropertyConverter(txtxMgr);
+                    plugin.Execute();
+                }
+                else if (dialog.SelectedOperation == Window1.Operation.Encapsulate)
+                {
+                    var plugin = new FieldConverter(txtxMgr);
+                    plugin.Execute();
+                }
+            }
+            catch (Exception ex)
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    ex.Message,
+                    "PropMan",
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+
+        object GetService<T>()
+        {
+            return this.ServiceProvider.GetServiceAsync(typeof(T))?.Result;
+        }
+    }
+}
