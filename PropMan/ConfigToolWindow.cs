09f9688975139745791a48a925f922532b0f017f
oleg.shilo <oleg.shilo@gmail.com>
2022-03-01 10:44:40 +1100

    Release v1.0.1.0 - Migrated to VS2022 - Added UTest - Added support for field initializers

--- PropMan/ConfigToolWindow.cs
+++ PropMan/ConfigToolWindow.cs
@@ -1,6 +1,6 @@
-﻿using Microsoft.VisualStudio.Shell;
-using System;
+﻿using System;
 using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
 
 namespace PropMan
 {
@@ -23,7 +23,7 @@ namespace PropMan
         /// </summary>
         public ConfigToolWindow() : base(null)
         {
-            this.Caption = "ConfigToolWindow";
+            this.Caption = "PropMan Config";
 
             // This is the user control hosted by the tool window; Note that, even if this class implements IDisposable,
             // we are not calling Dispose on this object. This is because ToolWindowPane calls Dispose on
